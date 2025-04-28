3d5cd7b08cc6856d6694cd762726b03c0ddd5c98
Lidortubul1 <tubul65431@gmail.com>
2025-04-28 10:57:36 +0300

    המשך תיקון של הקומפוננת מוצרים
    שינוי הנתיב לנתיבים הנכונים

---
+++ src/components/productList/productList.jsx
@@ -0,0 +1,11 @@
+import styles from "./ProductList.module.css";
+
+function ProductList() {
+  return (
+    <div className={styles.container}>
+      <h1>ProductList</h1>
+    </div>
+  );
+}
+
+export default Footer;
